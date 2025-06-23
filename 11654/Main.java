4e4f8f25d406e5b9118b2a059f6efb4050511849
jisoo0507 <jisoo0507@naver.com>
2025-06-23 21:47:57 +0900

    boj: 11654 - 아스키코드 문제 풀이

---
+++ 11654/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = br.read();
+        System.out.println(input);
+    }
+}
